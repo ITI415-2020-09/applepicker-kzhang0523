a0d62a5c1f5cd67840d44932f62f0eab3c52d138
kzhang0523 <70768441+kzhang0523@users.noreply.github.com>
2020-09-17 19:31:52 -0400

    Update Script and Add layers

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -18,12 +18,37 @@ public class AppleTree : MonoBehaviour
 
     void Start()
     {
-        
+        Invoke("DropApple", 2f);
+    }
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("AppleDrop", secondsBetweenAppleDrops);
     }
 
     
     void Update()
     {
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if(pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);
+        } else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);
+        }
         
+
+    }
+    void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1;
+        }
     }
 }
