24e9fdc6997a3768d8fd4cb00fb9f4f6d8b9e24d
kzhang0523 <70768441+kzhang0523@users.noreply.github.com>
2020-09-18 00:47:24 -0400

    Destroy Basket When Apple is Dropped

--- Assets/Scripts/Apple.cs
+++ Assets/Scripts/Apple.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Apple : MonoBehaviour
 {
+    [Header("Set In Inspector")]
+
     public static float bottomY = -20f;
   
 
@@ -13,6 +15,11 @@ public class Apple : MonoBehaviour
         if(transform.position.y < bottomY)
         {
             Destroy(this.gameObject);
+
+            //get a reference to the ApplePicker component of the Main Camera
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();
+            //Call the public AppleDestryed() method of apScript
+            apScript.AppleDestroyed();
         }
     }
 }
--- Assets/Scripts/ApplePicker.cs
+++ Assets/Scripts/ApplePicker.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour
 {
@@ -9,18 +10,37 @@ public class ApplePicker : MonoBehaviour
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
 
     void Start()
     {
         for(int i=0; i<numBaskets; i++)
         {
+            basketList = new List<GameObject>();
             GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY * i);
             tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
         }
     }
-
+    public void AppleDestroyed()
+    {
+        //Destroy all of the falling apples
+        GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
+        foreach (GameObject tGO in tAppleArray)
+        {
+            Destroy(tGO);
+        }
+        //Destroy one of the Baskets
+        //Get the indec of the last basket in basketlist
+        int basketIndex = basketList.Count - 1;
+        //Get reference to that Basket GameObject
+        GameObject tBasketGO = basketList[basketIndex];
+        //Remove the Basket from the list and destroy the GameObject
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGO);
+    }
     void Update()
     {
         
